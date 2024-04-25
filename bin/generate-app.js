117df0f531bfda1a9d81284e28d19f599a3ef492
narip <narip@github.com>
2024-04-26 02:08:30 +0900

    feat: script 추가

---
+++ bin/generate-app.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const GIT_REPO = "https://github.com/NariP/react-template.git";
+
+async function main() {
+    try {
+        console.log("Downloading files...");
+        execSync(`git clone --depth 1 ${GIT_REPO} ${projectPath}`); // boiler Plate repo clone
+
+        if (projectName !== ".") {
+            process.chdir(projectPath); // cd project path
+        }
+
+        console.log("Installing dependencies...");
+        execSync("pnpm install"); // install packages
+
+        console.log("Removing useless files");
+        execSync("npx rimraf ./.git"); // 보일러플레이트의 git과 관련된 내용 제거
+
+        console.log("The installation is done, this is ready to use !");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
